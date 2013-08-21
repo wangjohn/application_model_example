c2391c9aa43ab6e2e7e284c6fc9cb37102db4cc6
wangjohn <wangjohn@mit.edu>
2013-08-20 20:49:16 -0400

    Using instance exec.

--- example.rb
+++ example.rb
@@ -1,6 +1,16 @@
 require 'active_record'
 require 'minitest/autorun'
 
+# In this use case, there are two modules, <tt>LegacyModule</tt> and <tt>NewModule</tt>
+# which both have a database connection. The <tt>LegacyModule</tt> has a connection
+# to the legacy database, while the <tt>NewModule</tt> has a connection to the new
+# database.
+#
+# When we look at the <tt>ActiveRecord</tt> constants, we see that the <tt>table_name_prefix</tt>
+# constant should be different depending on which module things are coming from. When
+# inside of the <tt>LegacyModule</tt>, the prefix should be "legacy". When inside of
+# the <tt>NewModule</tt>, the prefix should be "new". Otherwise, the default prefix is
+# the empty string when not inside either of these two modules.
 class ApplicationModelTest < MiniTest::Unit::TestCase
   module LegacyModule
     class LegacyDatabase < ActiveRecord::Base
@@ -20,7 +30,7 @@ class ApplicationModelTest < MiniTest::Unit::TestCase
 
   def test_correct_name_prefixes
     assert_equal "", ActiveRecord::Base.table_name_prefix
-    assert_equal "legacy", LegacyModule.instance_eval(ActiveRecord::Base.table_name_prefix)
-    assert_equal "new", NewModule.instance_eval(ActiveRecord::Base.table_name_prefix)
+    assert_equal "legacy", LegacyModule.instance_exec { ActiveRecord::Base.table_name_prefix }
+    assert_equal "new", NewModule.instance_exec { ActiveRecord::Base.table_name_prefix }
   end
 end

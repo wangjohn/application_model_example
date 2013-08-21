bc8d97bbd0a847446546d443eb64604adadebc89
wangjohn <wangjohn@mit.edu>
2013-08-21 05:19:50 -0400

    Changed the tests.

--- example.rb
+++ example.rb
@@ -2,37 +2,59 @@ require '~/rails/activerecord/lib/active_record'
 require '~/rails/activesupport/lib/active_support'
 require 'minitest/autorun'
 
-# In this use case, there are two modules, <tt>LegacyModule</tt> and <tt>NewModule</tt>
-# which both have a database connection. The <tt>LegacyModule</tt> has a connection
-# to the legacy database, while the <tt>NewModule</tt> has a connection to the new
-# database.
-#
-# When we look at the <tt>ActiveRecord</tt> constants, we see that the <tt>table_name_prefix</tt>
-# constant should be different depending on which module things are coming from. When
-# inside of the <tt>LegacyModule</tt>, the prefix should be "legacy". When inside of
-# the <tt>NewModule</tt>, the prefix should be "new". Otherwise, the default prefix is
-# the empty string when not inside either of these two modules.
+module LegacyModule
+  class LegacyApplicationModel < ActiveRecord::ApplicationModel
+    configs_from(LegacyModule)
+  end
+
+  class LegacyApplicationModelTest < MiniTest::Unit::TestCase
+    def setup
+      LegacyApplicationModel.table_name_prefix = "legacy"
+    end
+
+    def test_default_prefix
+      assert_equal "legacy", ActiveRecord::ApplicationModel.get_config("table_name_prefix", self)
+    end
+
+    def test_changing_prefix_using_constant
+      LegacyApplicationModel.table_name_prefix = "hello"
+      assert_equal "hello", ActiveRecord::ApplicationModel.get_config("table_name_prefix", self)
+    end
+
+    def test_setting_prefix
+      ActiveRecord::ApplicationModel.set_config("table_name_prefix", "noooo", self)
+      assert_equal "noooo", ActiveRecord::ApplicationModel.get_config("table_name_prefix", self)
+    end
+  end
+end
+
+module NewModule
+  class NewApplicationModel < ActiveRecord::ApplicationModel
+    configs_from(NewModule)
+  end
+
+  class NewApplicationModelTest < MiniTest::Unit::TestCase
+    def setup
+      NewApplicationModel.table_name_prefix = "new"
+    end
+
+    def test_default_prefix
+      assert_equal "new", ActiveRecord::ApplicationModel.get_config("table_name_prefix", self)
+    end
+
+    def test_changing_prefix_using_constant
+      NewApplicationModel.table_name_prefix = "hello"
+      assert_equal "hello", ActiveRecord::ApplicationModel.get_config("table_name_prefix", self)
+    end
+
+    def test_setting_prefix
+      ActiveRecord::ApplicationModel.set_config("table_name_prefix", "hihoo", self)
+      assert_equal "hihoo", ActiveRecord::ApplicationModel.get_config("table_name_prefix", self)
+    end
+  end
+end
+
 class ApplicationModelTest < MiniTest::Unit::TestCase
-  module LegacyModule
-    class LegacyApplicationModel < ActiveRecord::ApplicationModel
-      configs_from(LegacyModule)
-
-      self.table_name_prefix = "legacy"
-    end
-
-    p ActiveRecord::ApplicationModel.get_config("table_name_prefix", self)
-  end
-
-  module NewModule
-    class NewApplicationModel < ActiveRecord::ApplicationModel
-      configs_from(NewModule)
-
-      self.table_name_prefix = "new"
-    end
-  end
-
-  ActiveRecord::ApplicationModel.table_name_prefix = ""
-
   def test_default_prefix
     assert_equal "", ActiveRecord::ApplicationModel.table_name_prefix
   end
